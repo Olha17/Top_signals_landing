5ca51a26bcad03f47bd54fb6c18afc166113fd02
Olha <olga.knyaginya96@gmail.com>
2022-11-24 10:24:59 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+let saveFile = () => {
+
+  // Get the data from each element on the form.
+  const name = document.getElementById('name');
+  const age = document.getElementById('phone');
+  const email = document.getElementById('email');
+
+  // This variable stores all the data.
+  let data = 
+      '\r Name: ' + name.value + ' \r\n ' + 
+      'Phone: ' +phone.value + ' \r\n ' + 
+      'Email: ' + email.value + ' \r\n ';
+
+  // Convert the text to BLOB.
+  const textToBLOB = new Blob([data], { type: 'text/plain' });
+  const sFileName = 'leads.txt';	   // The file to save the data.
+
+  let newLink = document.createElement("a");
+  newLink.download = sFileName;
+
+  if (window.webkitURL != null) {
+    newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+  }
+  else {
+    newLink.href = window.URL.createObjectURL(textToBLOB);
+    newLink.style.display = "none";
+    document.body.appendChild(newLink);
+  }
+
+  newLink.click(); 
+}
